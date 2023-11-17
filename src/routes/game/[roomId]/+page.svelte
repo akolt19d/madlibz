5123acc29292e5e64aeff04e24abc8e03d0ecf1c
akolt19d <a.kolt.19d@gmail.com>
2023-11-17 17:04:02 +0100

    Added selecting random story

--- src/routes/game/[roomId]/+page.svelte
+++ src/routes/game/[roomId]/+page.svelte
@@ -2,7 +2,7 @@
     import { goto } from "$app/navigation";
     import Loader from "$lib/components/Loader.svelte";
     import { globalSocket } from "$lib/socket.js";
-    import { TabGroup, Tab, ProgressRadial, getModalStore, getToastStore } from '@skeletonlabs/skeleton';
+    import { TabGroup, Tab, ProgressRadial, getModalStore, getToastStore, InputChip } from '@skeletonlabs/skeleton';
     import StoryCard from "$lib/components/StoryCard.svelte";
     import { fade } from "svelte/transition";
     import { writable } from "svelte/store";
@@ -31,6 +31,12 @@
     let gameVariables = writable({})
     let title = ""
     let text = ""
+    let filters = {
+        tags: [],
+        original: null,
+        nsfw: null
+    }
+    $: console.log(filters)
     let storyArea
     $: if(storyArea) {
         processText()
@@ -90,6 +96,15 @@
         socket.emit("selectingStory", data.roomId, story)
     }
 
+    function handleInvalidInputChip() {
+        showErrorToast(toastStore, "The tag you tried to enter is invalid.")
+    }
+
+    function validateInputChip(value) {
+        let regex = /^[a-zA-Z0-9%_.~-]+$/g
+        return regex.test(value)
+    }
+
     function inputCustomStory() {
         let regex = /_([^_]+)_/g
         let invalidRegex = /([<>])/g
@@ -106,6 +121,29 @@
         }
         socket.emit("selectingStory", data.roomId, story)
         console.log(story)
+    }
+
+    function createQueryString() {
+        let { original, nsfw, tags } = filters
+        let queryArr = []
+        if(original !== null)
+            queryArr.push(`original=${original}`)
+        if(nsfw !== null)
+            queryArr.push(`nsfw=${nsfw}`)
+        if(tags.length > 0)
+            queryArr.push(`tags=${tags.join(",")}`)
+
+        return `?${queryArr.join("&")}`
+    }
+
+    async function getRandomStory() {
+        const queryString = createQueryString()
+        const res = await fetch(`/api/story${queryString}`)
+        story = await res.json()
+        if(story)
+            socket.emit("selectingStory", data.roomId, story)
+        else 
+            showErrorToast(toastStore, "A story with the given filters doesn't exist.")
     }
 
     async function getStory(id) {
@@ -165,7 +203,30 @@
                     <div class="grid grid-cols-2">
                         {#if gameOption == 0}
                             <div class="h-full w-fit px-4">
-                                <button class="btn-secondary">Select</button>
+                                <h3 class="h3">Select random story</h3>
+                                <div class="card mt-1 mb-4 p-8 py-4 variant-filled-secondary border-4 border-secondary-700 shadow-[-.25rem_.25rem_0_.25rem_#9e5f8d]">
+                                    <p class="font-bold">You can specify filters for the story:</p>
+                                    <div class="flex justify-evenly text-center gap-2 my-2">
+                                        <div class="flex flex-col gap-1">
+                                            <label for="select-nsfw">NSFW</label>
+                                            <select bind:value={filters.nsfw} id="select-nsfw" class="select">
+                                                <option value={true}>Yes</option>
+                                                <option value={false}>No</option>
+                                                <option value={null}>No preference</option>
+                                            </select>
+                                        </div>
+                                        <div class="flex flex-col gap-1">
+                                            <label for="select-original">Original</label>
+                                            <select bind:value={filters.original} id="select-original" class="select">
+                                                <option value={true}>Yes</option>
+                                                <option value={false}>No</option>
+                                                <option value={null}>No preference</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <InputChip placeholder="Enter tags..." validation={validateInputChip} on:invalid={handleInvalidInputChip} bind:value={filters.tags} max={10} chips="variant-filled-primary border-4 border-primary-700 hover:!filter-none" />
+                                </div>
+                                <button class="btn-primary" on:click={getRandomStory}>Select</button>
                                 {#if !story}
                                     <p class="h3 mt-2 text-error-500 font-bold">No story selected.</p>
                                 {:else}
@@ -181,7 +242,7 @@
                             </div>
                         {:else if gameOption == 1}
                             <div class="h-full w-fit px-4">
-                                <label for="story-id">Enter story identificator:</label>
+                                <label for="story-id" class="h3">Enter story identificator:</label>
                                 <input type="text" id="story-id" placeholder="Story ID" class="input-primary my-2" maxlength="24" bind:value={storyId}>
                                 <button class="btn-secondary" disabled={!(storyId.length > 0)} on:click={() => { getStory(storyId) }}>Select</button>
                                 {#if !story}
